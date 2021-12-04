66e22d7a82c8cb61dbef202990787a7f7529939a
Janine Veronique <jtv258@nyu.edu>
2021-12-04 08:17:33 -0400

    city

---
+++ script.js
@@ -0,0 +1,171 @@
+let img;
+var sunrise = 500;
+var height1;
+let t;
+let drop;
+let drops = [];
+let snowflakes = [];
+let sun = [];
+let rain= [];
+const numOfDrops = 1000;
+let showSunrise= false;
+let showPixel= false;
+
+function preload() {
+  img = loadImage("assets/download.jpg");
+}
+
+function setup() {
+  createCanvas(400, 500);
+  fill(240);
+  noStroke();
+  background(100);
+  image(img, 0, 0, 400, 500);
+
+  for (let i = 0; i < 500; i++) {
+    snowflakes.push(new snowflake()); // append snowflake object
+  }
+
+    for (let i = 0; i < numOfDrops; i++) {
+    rain.push (new Raindrop());
+  }
+}
+
+function draw() {
+  background(100);
+  image(img, 0, 0, 400, 500);
+
+  if (showSunrise==true){
+  c = color(255, 212, 0);
+  fill(c);
+  ellipseMode(CORNER);
+  ellipse(50, sunrise, 50);
+  if (sunrise > 50) sunrise--;
+  // image(img, 0, 0, 400, 500);
+  t = frameCount / 60; // update time
+  //console.log(t)
+  }
+  // create a random number of snowflakes each frame
+  // for (let i = 0; i < random(5); i++) {
+  //   snowflakes.push(new snowflake()); // append snowflake object
+  // }
+
+  // loop through snowflakes with a for..of loop
+
+  for (let flake of snowflakes) {
+    if (flake.run == true) {
+      //flake.update2(); // update snowflake position
+      flake.update(t);
+      flake.display(); // draw snowflake
+    }
+  }
+
+      for (let drop of rain) {
+        if (drop.run==true){
+        drop.fall();
+        drop.draw();
+        }
+      }
+    }
+  if(showPixel==true){
+   for(let x=0;x<400;x+=5){
+     for(let y=0;y<500;y+=5){
+   let clr = img.get(x, y);
+
+     let r = red(clr) ;
+     let g = green(clr);
+     let b = blue(clr);
+
+     fill(r,g,b);
+       noStroke();
+       ellipse(x,y,5,5);
+
+     }
+   }
+   }
+
+
+class snowflake {
+  constructor(x, y) {
+    this.run = false;
+    // initialize coordinates
+    this.posX = 0;
+    //this.posX = random(width);
+    this.posY = random(-50, 0);
+    this.initialangle = random(0, 2 * PI);
+    this.size = random(2, 5);
+
+    // radius of snowflake spiral
+    // chosen so the snowflakes are uniformly spread out in area
+    this.radius = sqrt(random(pow(width / 2, 2)));
+
+    this.update = function (time) {
+      // x position follows a circle
+      let w = 0.6; // angular speed
+      let angle = w * time + this.initialangle;
+      this.posX = width / 2 + this.radius * sin(angle);
+
+      // different size snowflakes fall at slightly different y speeds
+      this.posY += pow(this.size, 0.5);
+
+      // delete snowflake if past end of screen
+      if (this.posY > height) {
+        let index = snowflakes.indexOf(this);
+        snowflakes.splice(index, 1);
+      }
+    };
+
+    this.update2 = function () {
+      this.posY += 1;
+    };
+
+    this.display = function () {
+      ellipse(this.posX, this.posY, this.size);
+      //ellipse(this.posX,this.posY,2,2);
+    };
+  }
+}
+
+class Raindrop{
+  constructor(x, y ){
+    this.run= false;
+    this.x = random(0, width);
+    this.y = random(-height, 0);
+    this.size = random(1.5,8);
+    if(this.size > 3){  this.size = random(1.5,8) }
+    this.speed = this.size / 0.5;
+    this.dropThickness = floor(this.size/3.8);
+    if(this.dropThickness < 1){ this.dropThickness = 1; }
+  }
+    fall() {
+    this.y += this.speed;
+  }
+
+  draw() {
+    strokeWeight(this.dropThickness);
+    stroke(255);
+    line(this.x, this.y, this.x, this.y - this.size * 2);
+  }
+
+}
+function fall() {
+  console.log("button works!");
+  for (let i = 0; i < snowflakes.length; i++) {
+    snowflakes[i].run = true;
+  }
+}
+
+ function surprise() {
+   console.log("button works!");
+    showPixel=true;
+  }
+
+function sunriseShow(){
+  showSunrise=true;
+}
+function rainFall(){
+  console.log("button works!");
+  for (let i = 0; i < numOfDrops; i++){
+   rain[i].run = true;
+  }
+}
